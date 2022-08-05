8a2fae969e0be0b1a4779afe582e12510cbb1e83
nihadmm <nihadmm@code.edu.az>
2022-08-05 12:29:20 +0400

    HomeTask2.1

---
+++ HomeTask2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeTask2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] odd = { 1, 3, 5, 7, 9 };
+            int[] even = { 2, 4, 6, 8, 10 };
+            int sum = 0;
+            for (int i = 0; i < odd.Length; i++)
+            {
+                Console.WriteLine(odd[i]);
+                sum += odd[i];
+                
+            }
+            Console.WriteLine("sum of oddnumber: "+sum);
+            Console.WriteLine("---------------------------");
+            for (int a = 0; a < even.Length; a++)
+            {
+                Console.WriteLine(even[a]);
+            }
+        }
+    }
+}
